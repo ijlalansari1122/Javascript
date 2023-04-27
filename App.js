9e1b34daf781385ebc739d0a7ac67bcfa7d3484f
ijlalansari1122 <ansariijlal90@gmail.com>
2023-04-27 17:09:32 +0500

    events in js

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-const arr =[5,7,45,32,124,6]
-arr.sort(comparenums);
-function comparenums(firstnum ,secondnum) {
-if (firstnum ===3) {
-    return -1;
-}
-if (secondnum ==3) {
-    return -1;
-}
 
-    return firstnum - secondnum;
-}
-console.log(arr);
+
+ 
+  
+  const button = document.querySelector('button');
+
+
+
+  button.addEventListener('click', setBColor);
+
+
+
+
+  function setBColor() {
+
+
+    document.body.style.backgroundColor = 'yellow';
+  }
+
+
+
