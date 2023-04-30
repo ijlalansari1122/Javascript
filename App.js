43750409e650ada33c60bc568f50d3d9b0b454fb
ijlalansari1122 <ansariijlal90@gmail.com>
2023-04-30 17:12:04 +0500

    Dom manipulation

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 
+const listitem = document.querySelectorAll('li');
 
+const p = document.createElement('p');
+p.textContent = 'hello world'
+
+ document.body.append(p)
+
+ const parent = document.querySelector('ol')
+ const fragment =document.createDocumentFragment()
  
-  
-  const button = document.querySelector('button');
+ for (let i = 0; i < 3; i++) {
+    const li = document.createElement('li')
+    li.textContent =   `list item with i=${i}`
+fragment.append(li)
+    
+}
+parent.append(fragment)
 
-
-
-  button.addEventListener('click', setBColor);
-
-
-
-
-  function setBColor() {
-
-
-    document.body.style.backgroundColor = 'yellow';
-  }
+console.log(listitem[0].textContent = 'hello ask; asm')
 
 
 
