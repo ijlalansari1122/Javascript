39427ffaa6200219a71f9c3947a87b8e68514ba8
ijlalansari1122 <ansariijlal90@gmail.com>
2023-04-30 23:12:35 +0500

    hi there

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
+const button =document.querySelector('button');
+button.addEventListener('click', onclick);
+function onclick() {
+    console.log(event)
+    console.log(event.type)
+    console.log(event.target)
 
-const listitem = document.querySelectorAll('li');
-
-const p = document.createElement('p');
-p.textContent = 'hello world'
-
- document.body.append(p)
-
- const parent = document.querySelector('ol')
- const fragment =document.createDocumentFragment()
- 
- for (let i = 0; i < 3; i++) {
-    const li = document.createElement('li')
-    li.textContent =   `list item with i=${i}`
-fragment.append(li)
-    
+    console.log('You clicked the button')
 }
-parent.append(fragment)
-
-console.log(listitem[0].textContent = 'hello ask; asm')
-
-
-
